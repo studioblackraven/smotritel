fc196d55c47d1c1738514f0eb10e7abef14ccfd5
RhenarSBR <contact@studioblackraven.fr>
2016-04-20 01:22:40 +0200

    Version 0.3
    Ajout des Convars de mApex

--- lua/mpgh/cl_mapexcommand.lua
+++ lua/mpgh/cl_mapexcommand.lua
@@ -9,6 +9,8 @@ include("lua/base/ban.lua")
 
 ply = LocalPlayer()
 
+-- COMMANDES --
+
 function mapexcommand(ply)
 
 if ply:ConCommand("mapex_wall") then
@@ -79,3 +81,106 @@ end
 end
 
 hook.Add("mApexCommand","mapexcommand",mapexcommand)
+
+-- CONVARS --
+
+function mapexconvar(ply)
+
+	if ply:GetConVar("mapex_showadmins") then
+
+		hook.Call("Ban")
+	end
+		
+	if ply:GetConVar("mapex_rpgod") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_showspecs") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_sniper") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_weed") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_coke") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_gmodz") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_norecoil") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_lazer") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_dd") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_box") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_c4") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_nospread") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_showspec") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_antiafk") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_autoreload") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_perp_playerinfo") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_ulxgag") then
+
+		hook.Call("Ban")
+	end
+
+	if ply:GetConVar("mapex_speedhack_speed") then
+
+		hook.Call("Ban")
+	end
+
+end	
+
+hook.Add("mApexConvar","mapexconvar", mapexconvar)
